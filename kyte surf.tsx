e0b00bd5009cf54b29fdd34a24dc2f2d51caf510
niconiki <n.lazzati@gmail.com>
2021-03-26 02:25:07 +0100

    Upload file
    Upload file

---
+++ kyte surf.tsx
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="kyte_animated" tilewidth="32" tileheight="32" tilecount="66" columns="11">
+ <image source="kyte_animated.png" width="352" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="22" duration="120"/>
+   <frame tileid="44" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="23" duration="120"/>
+   <frame tileid="45" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="2">
+  <animation>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="24" duration="120"/>
+   <frame tileid="46" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="3">
+  <animation>
+   <frame tileid="3" duration="120"/>
+   <frame tileid="25" duration="120"/>
+   <frame tileid="47" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="4">
+  <animation>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="26" duration="120"/>
+   <frame tileid="48" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="5" duration="120"/>
+   <frame tileid="27" duration="120"/>
+   <frame tileid="49" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="6">
+  <animation>
+   <frame tileid="6" duration="120"/>
+   <frame tileid="28" duration="120"/>
+   <frame tileid="50" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="120"/>
+   <frame tileid="29" duration="120"/>
+   <frame tileid="51" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="120"/>
+   <frame tileid="30" duration="120"/>
+   <frame tileid="52" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="120"/>
+   <frame tileid="31" duration="120"/>
+   <frame tileid="53" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="120"/>
+   <frame tileid="32" duration="120"/>
+   <frame tileid="54" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="11">
+  <animation>
+   <frame tileid="11" duration="120"/>
+   <frame tileid="33" duration="120"/>
+   <frame tileid="55" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="12">
+  <animation>
+   <frame tileid="12" duration="120"/>
+   <frame tileid="34" duration="120"/>
+   <frame tileid="56" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="13">
+  <animation>
+   <frame tileid="13" duration="120"/>
+   <frame tileid="35" duration="120"/>
+   <frame tileid="57" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <animation>
+   <frame tileid="14" duration="120"/>
+   <frame tileid="36" duration="120"/>
+   <frame tileid="58" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="15" duration="120"/>
+   <frame tileid="37" duration="120"/>
+   <frame tileid="59" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="120"/>
+   <frame tileid="38" duration="120"/>
+   <frame tileid="60" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="17">
+  <animation>
+   <frame tileid="17" duration="120"/>
+   <frame tileid="39" duration="120"/>
+   <frame tileid="61" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="18">
+  <animation>
+   <frame tileid="18" duration="120"/>
+   <frame tileid="40" duration="120"/>
+   <frame tileid="62" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="19">
+  <animation>
+   <frame tileid="19" duration="120"/>
+   <frame tileid="41" duration="120"/>
+   <frame tileid="63" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="20" duration="120"/>
+   <frame tileid="42" duration="120"/>
+   <frame tileid="64" duration="120"/>
+  </animation>
+ </tile>
+ <tile id="21">
+  <animation>
+   <frame tileid="21" duration="120"/>
+   <frame tileid="43" duration="120"/>
+   <frame tileid="65" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
