210f06601b38122fec3bd41023868d737f21ef12
niconiki <n.lazzati@gmail.com>
2021-04-08 23:04:31 +0200

    Release finale
    Release finale

---
+++ animated.tsx
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="animated" tilewidth="32" tileheight="32" tilecount="14" columns="14">
+ <image source="../../Assets Tile/SERENE_VILLAGE_REVAMPED/Animated stuff/water_waves_32x32.png" width="448" height="32"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="120"/>
+   <frame tileid="1" duration="120"/>
+   <frame tileid="2" duration="120"/>
+   <frame tileid="3" duration="120"/>
+   <frame tileid="4" duration="120"/>
+   <frame tileid="5" duration="120"/>
+   <frame tileid="6" duration="120"/>
+   <frame tileid="7" duration="120"/>
+   <frame tileid="8" duration="120"/>
+   <frame tileid="9" duration="120"/>
+   <frame tileid="10" duration="120"/>
+   <frame tileid="11" duration="120"/>
+   <frame tileid="12" duration="120"/>
+   <frame tileid="13" duration="120"/>
+  </animation>
+ </tile>
+</tileset>
