6479d27e70aa39b5523be25fa66c46d04e236ed0
niconiki <n.lazzati@gmail.com>
2021-11-21 02:22:46 +0100

    fix texture

---
+++ reception_animated.tsx
@@ -0,0 +1,82 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="reception_animated" tilewidth="32" tileheight="32" tilecount="42" columns="14">
+ <image source="reception_animated.png" width="448" height="96"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="6" duration="100"/>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="12" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="5" duration="100"/>
+   <frame tileid="7" duration="100"/>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="13" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="14" duration="100"/>
+   <frame tileid="16" duration="100"/>
+   <frame tileid="18" duration="100"/>
+   <frame tileid="20" duration="100"/>
+   <frame tileid="22" duration="100"/>
+   <frame tileid="24" duration="100"/>
+   <frame tileid="26" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="15" duration="100"/>
+   <frame tileid="17" duration="100"/>
+   <frame tileid="19" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="25" duration="100"/>
+   <frame tileid="27" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="30" duration="100"/>
+   <frame tileid="32" duration="100"/>
+   <frame tileid="34" duration="100"/>
+   <frame tileid="36" duration="100"/>
+   <frame tileid="38" duration="100"/>
+   <frame tileid="40" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+  <animation>
+   <frame tileid="29" duration="100"/>
+   <frame tileid="31" duration="100"/>
+   <frame tileid="33" duration="100"/>
+   <frame tileid="35" duration="100"/>
+   <frame tileid="37" duration="100"/>
+   <frame tileid="39" duration="100"/>
+   <frame tileid="41" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
